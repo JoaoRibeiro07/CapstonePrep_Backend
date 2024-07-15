4c07e4b8c7ad0e056992b9b09d15ed2d28970851
Christian Butler <christianbutler@MacBook-Pro.local>
2024-07-15 07:16:00 -0600

    updated source files

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,27 @@
 const { route } = require("../shared/shared");
-const { register } = require("../controllers/userControllers");
+const { findUserWithToken } = require("../queries/userQueries");
+const {
+  register,
+  login,
+  displayAll,
+  deleteUser,
+  updateUser,
+} = require("../controllers/userControllers");
+
+const isLoggedIn = async (req, res, next) => {
+  try {
+    req.user = await findUserWithToken(req.headers.authorization);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
 route.post("/register", register);
+route.post("/login", login);
+route.get("/all_users", isLoggedIn, displayAll);
+route.delete("/:id/delete", isLoggedIn, deleteUser);
+route.put("/:id/change", isLoggedIn, updateUser);
 
 module.exports = route;
+module.exports.isLoggedIn = isLoggedIn;
