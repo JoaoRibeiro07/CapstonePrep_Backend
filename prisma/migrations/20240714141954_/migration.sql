1faa5b4f380f98ab0e5a05798f2d5d5f4744f4af
Christian Butler <christianbutler@MacBook-Pro.local>
2024-07-15 07:09:47 -0600

    feature 1 added

---
+++ prisma/migrations/20240714141954_/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "first name" TEXT NOT NULL,
+    "last name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
